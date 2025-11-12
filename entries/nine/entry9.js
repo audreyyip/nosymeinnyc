a32414f175156530d4ff499510259f0524f23086
audreyyip <audreyyiptszying@gmail.com>
2025-11-12 16:01:33 -0500

    updated entire website, included mobile responsive layout

--- entries/nine/entry9.js
+++ entries/nine/entry9.js
@@ -1,3 +1,8 @@
+window.addEventListener('scroll', () => {
+    document.querySelector("wrapper").style.transform = `translateY(${window.scrollY * 0.5}px)`;
+});
+
+
 const observer = new IntersectionObserver((entries)=> {
     entries.forEach((entry) => {
         console.log(entry);
@@ -12,3 +17,13 @@ const observer = new IntersectionObserver((entries)=> {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+
+// const [red, green, blue] = [69, 111, 225]
+// const section1 = document.querySelector('.section1')
+// /// let y = 1 + (window.scrollY || window.pageYOffset) / 150
+//   y = y < 1 ? 1 : y // ensure y is always >= 1 (due to Safari's elastic scroll)
+//   const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
+//   section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`///
