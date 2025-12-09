33d81ad47d8d6875df33b7210b6cc5d40c915931
audreyyip <audreyyiptszying@gmail.com>
2025-12-09 08:43:22 -0500

    fixed the js for changing the border image, fixed homepage buttons and added special style for the numbers, fixed the spacing and other paragraph styles for entry 12. entry 12 template ready

--- entries/twelve/entry12.js
+++ entries/twelve/entry12.js
@@ -1,16 +1,18 @@
 let body = document.body;
 let button = document.querySelector(".colorMode");
 let modeText = document.querySelector(".mode-text");
+let borderedElement = document.getElementById("bordered");
 
 function turnOnLight() {
   body.classList.toggle("light");
   button.classList.toggle("buttonOn");
   
-  // Change the text based on current mode
   if (body.classList.contains("light")) {
     modeText.textContent = "Light Mode";
+    document.getElementById("imageid").src="../../img/biscuit.png";
   } else {
     modeText.textContent = "Dark Mode";
+    document.getElementById("imageid").src="../../img/biscuit-white.png";
   }
 }
 
